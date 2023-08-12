5c9af348f904202092400cd12c349034621dfe85
exit <kalsijatin67@gmail.com>
2023-08-12 18:32:43 -0400

    init neural net training

---
+++ python/neural_network/model/model_training.py
@@ -0,0 +1,2 @@
+# train neural net
+# save model in this folder
---
+++ python/neural_network/neural_network_evaluate.py
@@ -0,0 +1,2 @@
+# use model saved in ./model to evaluate the ../../data/news_done.csv file and generate a positivity score
+# save that file as json and send to frontend
