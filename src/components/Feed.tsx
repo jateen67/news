5a401a0bd721ce74067eaa2fab599558a2424f64
exit <kalsijatin67@gmail.com>
2023-08-14 23:56:45 -0400

    added general feed structures

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -2,18 +2,44 @@ import display from "../python/data/display.json";
 
 export default function Feed() {
   return (
-    <div>
-      {display.map((article, idx) => {
-        return (
-          <div key={idx} className="article">
-            <p>{article.title}</p>
-            <p>{article.description}</p>
-            <p>{article.published}</p>
-            <p>{article.link}</p>
-            <p>{article.rating.toFixed(1)}</p>
+    <>
+      <div className="top-stories-container">
+        <div className="top-stories-label">
+          <p>TOP STORIES</p>
+        </div>
+        <div className="top-stories">
+          <div className="top-story">
+            <p>title</p>
+            <p>date</p>
+            <p>score</p>
           </div>
-        );
-      })}
-    </div>
+        </div>
+      </div>
+      <div className="latest-stories-container">
+        <div className="latest-stories-label">
+          <p>LATEST STORIES</p>
+        </div>
+        <div className="latest-stories">
+          <div className="latest-story">
+            <p>title</p>
+            <p>date</p>
+            <p>score</p>
+          </div>
+        </div>
+      </div>
+      <div className="articles-container">
+        {display.map((article, idx) => {
+          return (
+            <div key={idx} className="article">
+              <p>{article.title}</p>
+              <p>{article.description}</p>
+              <p>{article.published}</p>
+              <p>{article.link}</p>
+              <p>{article.rating.toFixed(1)}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
